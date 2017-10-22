6fbf8108ec06d8e2c53296ac0b744cf09b284d58
dudusandsten <dudusandsten@gmail.com>
2017-10-23 01:33:07 +0200

    Backup of exported database (you dont need this)

---
+++ backup/WebDKP.lua
@@ -0,0 +1,378 @@
+WebDKP_DkpTable = {
+["Abmi"] = { 
+     ["class"]="Hunter", 
+     ["dkp"]=1057.00, 
+     ["dkp_1"]=1057.00, 
+},
+["Arkemea"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=660.00, 
+     ["dkp_1"]=660.00, 
+},
+["Awsomenews"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=2924.00, 
+     ["dkp_1"]=2924.00, 
+},
+["Belcor"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=-96.00, 
+     ["dkp_1"]=-96.00, 
+},
+["Bigboned"] = { 
+     ["class"]="Hunter", 
+     ["dkp"]=891.00, 
+     ["dkp_1"]=891.00, 
+},
+["Bromona"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=2647.00, 
+     ["dkp_1"]=2647.00, 
+},
+["Cadux"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=1125.00, 
+     ["dkp_1"]=1125.00, 
+},
+["Cainer"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=1247.00, 
+     ["dkp_1"]=1247.00, 
+},
+["Carbuncle"] = { 
+     ["class"]="Hunter", 
+     ["dkp"]=436.00, 
+     ["dkp_1"]=436.00, 
+},
+["Delthar"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=630.00, 
+     ["dkp_1"]=630.00, 
+},
+["Deva"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=569.00, 
+     ["dkp_1"]=569.00, 
+},
+["Dorigen"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=1269.00, 
+     ["dkp_1"]=1269.00, 
+},
+["Easylife"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=510.00, 
+     ["dkp_1"]=510.00, 
+},
+["Emira"] = { 
+     ["class"]="Hunter", 
+     ["dkp"]=-50.00, 
+     ["dkp_1"]=-50.00, 
+},
+["Felz"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=1670.00, 
+     ["dkp_1"]=1670.00, 
+},
+["Filon"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=1797.00, 
+     ["dkp_1"]=1797.00, 
+},
+["Forvrangd"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=650.00, 
+     ["dkp_1"]=650.00, 
+},
+["Gnommynator"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=77.00, 
+     ["dkp_1"]=77.00, 
+},
+["Hurmelhej"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=1800.00, 
+     ["dkp_1"]=1800.00, 
+},
+["Injection"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=400.00, 
+     ["dkp_1"]=400.00, 
+},
+["Itsbait"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=413.00, 
+     ["dkp_1"]=413.00, 
+},
+["Johnsens"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=1159.00, 
+     ["dkp_1"]=1159.00, 
+},
+["Julosch"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=-22.00, 
+     ["dkp_1"]=-22.00, 
+},
+["Jungeltroll"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=1304.00, 
+     ["dkp_1"]=1304.00, 
+},
+["Kalven"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=1846.00, 
+     ["dkp_1"]=1846.00, 
+},
+["Kelom"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=1376.00, 
+     ["dkp_1"]=1376.00, 
+},
+["Klimpfan"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=2710.00, 
+     ["dkp_1"]=2710.00, 
+},
+["Knutisfix"] = { 
+     ["class"]="Druid", 
+     ["dkp"]=356.00, 
+     ["dkp_1"]=356.00, 
+},
+["Kylsten"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=60.00, 
+     ["dkp_1"]=60.00, 
+},
+["Larona"] = { 
+     ["class"]="Hunter", 
+     ["dkp"]=4363.00, 
+     ["dkp_1"]=4363.00, 
+},
+["Lazyspawn"] = { 
+     ["class"]="Druid", 
+     ["dkp"]=248.00, 
+     ["dkp_1"]=248.00, 
+},
+["Lmory"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=1162.00, 
+     ["dkp_1"]=1162.00, 
+},
+["Looz"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=1475.00, 
+     ["dkp_1"]=1475.00, 
+},
+["Lubbo"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=5.00, 
+     ["dkp_1"]=5.00, 
+},
+["Lyingmist"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=1713.00, 
+     ["dkp_1"]=1713.00, 
+},
+["Malthael"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=2068.00, 
+     ["dkp_1"]=2068.00, 
+},
+["Memphis"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=3324.00, 
+     ["dkp_1"]=3324.00, 
+},
+["Nakz"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=889.00, 
+     ["dkp_1"]=889.00, 
+},
+["Nerghul"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=1616.00, 
+     ["dkp_1"]=1616.00, 
+},
+["Nobody"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=40.00, 
+     ["dkp_1"]=40.00, 
+},
+["Oron"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=60.00, 
+     ["dkp_1"]=60.00, 
+},
+["Orthoron"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=363.00, 
+     ["dkp_1"]=363.00, 
+},
+["Pantheaa"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=2173.00, 
+     ["dkp_1"]=2173.00, 
+},
+["Potatomasher"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=2480.00, 
+     ["dkp_1"]=2480.00, 
+},
+["Presso"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=111.00, 
+     ["dkp_1"]=111.00, 
+},
+["Proljev"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=496.00, 
+     ["dkp_1"]=496.00, 
+},
+["Pruzer"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=288.00, 
+     ["dkp_1"]=288.00, 
+},
+["Quis"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=81.00, 
+     ["dkp_1"]=81.00, 
+},
+["Rerayne"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=-70.00, 
+     ["dkp_1"]=-70.00, 
+},
+["Roxigar"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=134.00, 
+     ["dkp_1"]=134.00, 
+},
+["Saitamac"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=524.00, 
+     ["dkp_1"]=524.00, 
+},
+["Salival"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=1841.00, 
+     ["dkp_1"]=1841.00, 
+},
+["Sandsten"] = { 
+     ["class"]="Druid", 
+     ["dkp"]=3471.00, 
+     ["dkp_1"]=3471.00, 
+},
+["Sesshomarou"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=-22.00, 
+     ["dkp_1"]=-22.00, 
+},
+["Species"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=1392.00, 
+     ["dkp_1"]=1392.00, 
+},
+["Spiru"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=1337.00, 
+     ["dkp_1"]=1337.00, 
+},
+["Stabzes"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=2110.00, 
+     ["dkp_1"]=2110.00, 
+},
+["Tentto"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=1160.00, 
+     ["dkp_1"]=1160.00, 
+},
+["Thobbe"] = { 
+     ["class"]="Rogue", 
+     ["dkp"]=469.00, 
+     ["dkp_1"]=469.00, 
+},
+["Tobitank"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=1499.00, 
+     ["dkp_1"]=1499.00, 
+},
+["Toriaz"] = { 
+     ["class"]="Shaman", 
+     ["dkp"]=1243.00, 
+     ["dkp_1"]=1243.00, 
+},
+["Trestugaar"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=190.00, 
+     ["dkp_1"]=190.00, 
+},
+["Tuntun"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=2178.00, 
+     ["dkp_1"]=2178.00, 
+},
+["Turbotigra"] = { 
+     ["class"]="Druid", 
+     ["dkp"]=1445.00, 
+     ["dkp_1"]=1445.00, 
+},
+["Tzui"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=332.00, 
+     ["dkp_1"]=332.00, 
+},
+["Veps"] = { 
+     ["class"]="Warrior", 
+     ["dkp"]=289.00, 
+     ["dkp_1"]=289.00, 
+},
+["Wingsy"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=1146.00, 
+     ["dkp_1"]=1146.00, 
+},
+["Worstlock"] = { 
+     ["class"]="Warlock", 
+     ["dkp"]=293.00, 
+     ["dkp_1"]=293.00, 
+},
+["Xaa"] = { 
+     ["class"]="Priest", 
+     ["dkp"]=280.00, 
+     ["dkp_1"]=280.00, 
+},
+["Xzu"] = { 
+     ["class"]="Mage", 
+     ["dkp"]=1777.00, 
+     ["dkp_1"]=1777.00, 
+},
+["Zahnet"] = { 
+     ["class"]="Hunter", 
+     ["dkp"]=25.00, 
+     ["dkp_1"]=25.00, 
+},
+} 
+
+WebDKP_Tables = {
+["Main Table"] = { 
+		["id"] = 1, 
+},
+}
+
+WebDKP_Loot = {
+}
+
+WebDKP_Alts = {
+}
+
+WebDKP_WebOptions = {
+["ZeroSumEnabled"] = 0,
+["CombineAlts"] = 0,
+["TiersEnabled"] = 0,
+["TierSize"] = 50,
+["LifetimeEnabled"] = 1,
+}
+
